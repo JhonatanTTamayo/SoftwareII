a072db3df1653bb493dd9a9e7a54f363eb0e2580
JhonatanTTamayo <91971169+JhonatanTTamayo@users.noreply.github.com>
2024-10-16 14:31:32 -0500

    Add files via upload

---
+++ CORTE_2/TALLER_14_PRUEBAS_UNITARIAS/Taller_14_Pruebas_Unitarias/src/taller_14_pruebas_unitarias/Taller_14_Pruebas_Unitarias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller_14_pruebas_unitarias;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Taller_14_Pruebas_Unitarias {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
