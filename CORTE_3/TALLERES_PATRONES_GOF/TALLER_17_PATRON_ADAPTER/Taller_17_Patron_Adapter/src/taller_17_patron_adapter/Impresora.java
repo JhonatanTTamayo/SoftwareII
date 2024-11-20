2a4e3ec19ba4217e2402fdc2232ccddeab8a5d1a
JhonatanTTamayo <91971169+JhonatanTTamayo@users.noreply.github.com>
2024-11-20 16:29:18 -0500

    Add files via upload
    Talleres

---
+++ CORTE_3/TALLERES_PATRONES_GOF/TALLER_17_PATRON_ADAPTER/Taller_17_Patron_Adapter/src/taller_17_patron_adapter/Impresora.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package taller_17_patron_adapter;
+
+/**
+ *
+ * @author jhonatan
+ */
+public interface Impresora {
+    void imprimir();
+}
+
